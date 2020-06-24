37ed83f76c42d1f0fd5053833bb2398f29ea206c
Starlyn <starlynfernandez1222@hotmail.com>
2020-06-24 16:46:30 -0400

    mi primer commit

---
+++ config/README.md
@@ -0,0 +1,9 @@
+## aaplicacion de comando
+
+este es el ejercicio del curso
+
+Recuerden instalar los paquetes de Node
+
+---
+
+## npm install
---
+++ por-hacer/por-hacer.js
@@ -0,0 +1,72 @@
+const fs = require("fs");
+
+let listadoPorHacer = [];
+
+const guardarDB = () => {
+  let data = JSON.stringify(listadoPorHacer);
+
+  fs.writeFile("db/data.json", data, (err) => {
+    if (err) throw new Error("No se pudo Grabar", err);
+  });
+};
+const cargaDB = () => {
+  try {
+    listadoPorHacer = require("../db/data.json");
+  } catch (error) {
+    listadoPorHacer = [];
+  }
+};
+
+const getListado = () => {
+  cargaDB();
+
+  return listadoPorHacer;
+};
+
+const crear = (descripcion) => {
+  cargaDB();
+  let porHacer = {
+    descripcion,
+    completado: false,
+  };
+  listadoPorHacer.push(porHacer);
+  guardarDB();
+  return porHacer;
+};
+
+const actualizar = (descripcion, completado = true) => {
+  cargaDB();
+
+  let index = listadoPorHacer.findIndex(
+    (tarea) => tarea.descripcion === descripcion
+  );
+
+  if (index >= 0) {
+    listadoPorHacer[index].completado = completado;
+    guardarDB();
+    return true;
+  } else {
+    return false;
+  }
+};
+
+const borrar = (descripcion) => {
+  cargaDB();
+  let nuevoListado = listadoPorHacer.filter((tarea) => {
+    return tarea.descripcion !== descripcion;
+  });
+  if (listadoPorHacer.length === nuevoListado.length) {
+    return false;
+  } else {
+    listadoPorHacer = nuevoListado;
+    guardarDB();
+    return true;
+  }
+};
+
+module.exports = {
+  crear,
+  getListado,
+  actualizar,
+  borrar,
+};
